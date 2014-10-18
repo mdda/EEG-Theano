18ebeea861d3727ba01ce9da106ad4cda6cb7e45
Martin Andrews <GitHub@mdda.net>
2014-10-18 15:18:27 +0800

    Thinking about normalization

--- src/graph.py
+++ src/graph.py
@@ -1,5 +1,27 @@
 import EEG
+import numpy as np
 
 p = EEG.EEG('Dog_2', 'interictal', 17)
 
-print p  
+#print np.shape(p.data)
+
+data = p.data
+norms = np.apply_along_axis(np.linalg.norm, 0, data)
+
+#eeg = data / norms
+
+exit(0)
+
+# p797 of pdf : matshow()
+
+import matplotlib.pyplot as plt
+
+data = p.data
+
+plt.figure(3);
+plt.plot(filtered[:, 0:20000].T + 5*np.arange(7,-1,-1));
+
+#plt.plot(np.zeros((512,8)) + 80*np.arange(7,-1,-1),'--',color='gray');
+#plt.yticks([]);
+#plt.axis('tight');
+#plt.legend(first['channels']);
