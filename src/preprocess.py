73bd2e1347f71de31ea0d7928e01b9c653685bf2
Martin Andrews <GitHub@mdda.net>
2014-10-24 01:44:10 +0800

    FFT output seems to be in correct shape

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,7 @@
 import EEG
 import numpy as np
 import scipy as sp
+from scipy import fftpack
 
 p = EEG.EEG('Dog_2', 'interictal', 17)
 #p = EEG.EEG('Patient_2', 'interictal', 17)
@@ -13,3 +14,10 @@ print p
 data = p.data
 #eeg = np.rollaxis(data, 1)
 
+signal_time  = 15.0 # in secs
+signal_length = int(p.sample_rate_in_hz * signal_time)
+
+signal_start = 0
+z = fftpack.rfft(p.data, n=signal_length, axis=1)
+
+print np.shape(z)
