fb35096f60b74bd8c0e8f906ccbf5814ee70a766
Martin Andrews <GitHub@mdda.net>
2014-10-25 01:26:40 +0800

    Binned complex FFT by matrix product

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,8 @@
 import EEG
 
 import numpy as np
-import scipy as sp
-from scipy import fftpack
+#import scipy as sp
+#from scipy import fftpack
 
 import math
 
@@ -22,15 +22,38 @@ signal_duration_min = 9.0 # in secs
 pow2 = math.log(p.sample_rate_in_hz * signal_duration_min)/math.log(2.)
 
 sample_length  = int(2.0 ** (int(pow2)+1))   # Rounds up  
-sample_start   = int(p.sample_rate_in_hz * 0)
-
 signal_duration = sample_length/p.sample_rate_in_hz
 print "Pow2: ", pow2
-print "Signal duration : %6.2f" % (signal_duration,)
+print "Signal duration : %6.2fsec " % (signal_duration,)
 
-z = fftpack.rfft(p.data[:, sample_start:], n=sample_length, axis=1)
+## Matrix that gathers FFT entries into buckets
+## Want buckets to be [0-0.000001 - 1.5 - 2.5 - 3.5 - ... - 48.5] Hz
+bin_array = range(0, 48)
+
+## http://docs.scipy.org/doc/numpy/reference/routines.fft.html#module-numpy.fft
+
+#freq = fftpack.rfftfreq(n=sample_length, d=1./p.sample_rate_in_hz)
+freq = np.fft.rfftfreq(n=sample_length, d=1./p.sample_rate_in_hz)
+print freq[0:100]
+
+bin_fft = np.zeros( (len(freq), len(bin_array)) )
+for i, bn in enumerate(bin_array):
+  a = np.where( (freq>(bn-0.5)) & (freq<(bn+0.5)) , 1, 0)
+  bin_fft[:,i] = a
+
+print bin_fft[0:20, 0:5]
+
+
+
+sample_start   = int(p.sample_rate_in_hz * 0)
+
+#z = fftpack.rfft(p.data[:, sample_start:], n=sample_length, axis=1)
+z = np.fft.rfft(p.data[:, sample_start:], n=sample_length, axis=1)
 print np.shape(z)
-#print z[0:1, 0:20]
+print z[0:1, 0:20]
 
-freq = fftpack.rfftfreq(n=sample_length, d=1./p.sample_rate_in_hz)
-print freq[0:20]
+binned = np.dot(z,bin_fft)
+#print binned[0:1, :]
+print binned[0, 0]
+
+#print np.sum(z[0,0:6]) # Works!
